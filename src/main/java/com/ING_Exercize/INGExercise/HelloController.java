ca6a71ddabc05dd71e61c2504f31ff9caf22918f
Straticiuc Mihai <mihai.straticiuc1995@gmail.com>
2024-11-15 12:56:32 -0800

    Second Commit

--- src/main/java/com/ING_Exercize/INGExercise/HelloController.java
+++ src/main/java/com/ING_Exercize/INGExercise/HelloController.java
@@ -14,6 +14,8 @@ public class HelloController {
 
     @GetMapping("/helloWorld")
     public String helloWorld() {
-        return "Greetings from Spring Boot!";
+        return "Hello from Spring Boot!";
     }
+
+
 }
